a282121a1a91591a02051c9de71a635b5cbbc1f7
lmarquez1020 <luismarquez@pursuit.org>
2023-09-19 12:58:14 -0400

    completed my assignment

--- index.js
+++ index.js
@@ -3,19 +3,14 @@
  * @param {*[]} array - An array of elements. Could be anything!
  * @param {function} callback - A callback that accepts a single argument. Returns a value.
  * @returns {*|undefined} The first element that causes the callback to return a truthy value. Otherwise, returns `undefined`.
- *
- * EXAMPLE:
- *  find([1, 2, 3], (element) => element > 1);
- *  //> 2
- *
- * EXAMPLE:
- *  find([1, 2, 3], (element) => element < 0);
- *  //> undefined
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+    }
   }
+  return undefined;
 }
 
 /**
@@ -23,19 +18,13 @@ function find(array, callback) {
  * @param {*[]} array - An array of elements. Could be anything!
  * @param {function} callback - A callback that accepts a single argument. Returns a value.
  * @returns {*[]} An array of filtered values. Potentially empty.
- *
- * EXAMPLE:
- *  filter([1, 2, 3], (element) => element > 1);
- *  //> [2, 3]
- *
- * EXAMPLE:
- *  filter([1, 2, 3], (element) => element < 0);
- *  //> []
  */
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      result.push(element);
+    }
   }
   return result;
 }
@@ -45,19 +34,11 @@ function filter(array, callback) {
  * @param {*[]} array - An array of elements. Could be anything!
  * @param {function} callback - A callback that accepts a single argument. Returns a value.
  * @returns {*[]} An array of transformed elements. The length of this array should be the same as the inputted array.
- *
- * EXAMPLE:
- *  map([1, 2, 3], (element) => element + 10);
- *  //> [11, 12, 13]
- *
- * EXAMPLE:
- *  map([], (element) => element < 0);
- *  //> []
  */
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element));
   }
   return result;
 }
@@ -66,18 +47,10 @@ function map(array, callback) {
  * Does not return anything. Passes each element of the array into the callback along with the index and the array, in that order.
  * @param {*[]} array - An array of elements. Could be anything!
  * @param {function} callback - A callback that accepts three arguments: element, index, and the entire array.
- *
- * EXAMPLE:
- *  forEach([10, 20, 30], (element, index, array) => {
- *    console.log(element, index, array.length)
- *  });
- *  //> 10 0 3
- *  //> 20 1 3
- *  //> 30 2 3
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
