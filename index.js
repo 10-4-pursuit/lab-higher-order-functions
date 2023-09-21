3a8205b0c6df663816bea521a43546381f09b310
SaraM09 <sayeramusfika@pursuit.org>
2023-09-21 19:01:09 -0400

    Sara's higher order functions lab

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-          callback(array[i], i, array);
+          callback(array[i], i, array);        
   }
 }
 
