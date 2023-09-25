95ff2ff3cde64558e49d223d363e687effd3bc62
AmiraFredericks <amirafredericks@pursuit.org>
2023-09-25 14:45:16 -0400

    higher order

--- index.js
+++ index.js
@@ -14,9 +14,11 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if(callback(element)) {
+      return element 
+    }
   }
-  console.log(array);
+ return undefined;
 }
 
 /**
@@ -36,7 +38,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if(callback(element)) {
+      result.push(element)
+    }
   }
   return result;
 }
@@ -58,7 +62,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+   result.push(callback(element))
   }
   return result;
 }
@@ -78,7 +82,8 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+   const element = array[i]
+   callback(element, i, array)
   }
 }
 
